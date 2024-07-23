84cc5020a700f51897c419fdc891d4ac9600a682
Mthunzi Tom <mthunzitom.10@gmail.com>
2024-07-23 10:05:43 +0200

    Refactored RestaurantImpl.ts

--- RestaurantImpl.ts
+++ RestaurantImpl.ts
@@ -1,19 +1,13 @@
 import { IRestaurant } from "./IRestaurant";
 
 export default class RestaurantImpl {
-    findRestaurant(restaurantsList: IRestaurant[], restaurant: string): IRestaurant {
+    findRestaurant(restaurantsList: IRestaurant[], restaurant: string): IRestaurant | boolean {
         for (let i = 0; i < restaurantsList.length; i++) {
             const currentRestaurant: IRestaurant = restaurantsList[i];
             if (currentRestaurant.name === restaurant) {
                 return currentRestaurant;
             };
         };
-        return {
-            name: "",
-            site: "",
-            email: "",
-            phone_number: 0,
-            menu_items: [],
-        }
+        return false;
     }
 }
